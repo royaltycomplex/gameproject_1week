a0d4f51f7f10bac7cc258c29b77218bd19c6d04f
Albert <38449884+royaltycomplex@users.noreply.github.com>
2018-04-19 02:12:06 -0600

    first steps to boss ai/patterns

---
+++ bullet hell __ 1 week/Assets/Scripts/BossPrototypeController.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossPrototypeController : MonoBehaviour {
+
+	private int waitToSpawn = 0;
+	private int spawnCount = 25;
+	private int cooldown = 0;
+	private float rotateRef;
+
+	public GameObject bullet;
+
+	// Use this for initialization
+	void Start () {
+		rotateRef = bullet.GetComponent<Mover>().rotate;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		waitToSpawn ++;
+		if (waitToSpawn >= spawnCount)
+		{
+			waitToSpawn = 0;
+			//if (cooldown == 0) {Pattern5();};
+			Pattern3();
+		}
+		if (cooldown != 0)
+		{
+			cooldown--;
+		}
+	}
+
+	void Pattern1 ()
+	{
+			bullet.GetComponent<Mover>().rotate = 0;
+			int i = 1;
+			while (i < 21)
+			{
+				Instantiate(bullet, transform.position, transform.rotation);
+				transform.Rotate(0.0f, 18.0f, 0.0f);
+				i++;
+			}
+			bullet.GetComponent<Mover>().rotate = rotateRef;
+			i = 0;
+	}
+
+	void Pattern2 ()
+	{
+			int i = 1;
+			while (i < 10)
+			{
+				Instantiate(bullet, transform.position, transform.rotation);
+				transform.Rotate(0.0f, 18.0f, 0.0f);
+				i++;
+			}
+			i = 0;
+	}
+
+	void Pattern3 ()
+	{
+			int i = 1;
+			while (i < 21)
+			{
+				Instantiate(bullet, transform.position, transform.rotation);
+				transform.Rotate(0.0f, 18.0f, 0.0f);
+				i++;
+			}
+			i = 0;
+	}
+
+	void Pattern4 ()
+	{
+			Instantiate(bullet, transform.position, transform.rotation);
+			transform.Rotate(0.0f, 18.0f, 0.0f);
+	}
+
+	void Pattern5 ()
+	{
+			int i = 1;
+			while (i < 21)
+			{
+
+				Instantiate(bullet, transform.position, transform.rotation);
+				transform.Rotate(0.0f, 18.0f, 0.0f);
+				i++;
+			}
+			i = 0;
+			cooldown = 60;
+	}
+}
---
+++ bullet hell __ 1 week/Assets/Scripts/Mover.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+	private Rigidbody rb;
+
+	public float speed;
+	public float rotate;
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+
+	void FixedUpdate ()
+	{
+		transform.Rotate(0.0f, rotate * Time.deltaTime, 0.0f);
+		rb.position += -(transform.forward) * Time.deltaTime * speed;
+	}
+}
