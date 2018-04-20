cccd199b268f7e0e05c82932a506a4246bcc2c0d
Albert <38449884+royaltycomplex@users.noreply.github.com>
2018-04-19 23:38:22 -0600

    shoot shoot destroy destroy

---
+++ bullet hell __ 1 week/Assets/Scripts/EnemyHP.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHP : MonoBehaviour {
+
+	public float hp;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (hp <= 0)
+		{
+			Destroy(gameObject);
+		}
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "Player Bullet")
+		{
+			hp--;
+			Destroy(other.gameObject);
+		}
+	}
+}
---
+++ bullet hell __ 1 week/Assets/Scripts/EnemyMoveA.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMoveA : MonoBehaviour 
+{
+
+	public GameObject pattern;
+	public float speed;
+	public float rotate;
+
+	private int moveBeforeShoot;
+	private int waitWhileShoot;
+	private int frameCounter = 0;
+	private bool shoot = false;
+	private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () 
+	{
+		rb = GetComponent<Rigidbody>();
+		transform.Rotate(0.0f, rotate, 0.0f);
+		moveBeforeShoot = 120;
+		waitWhileShoot = 180;
+//		Instantiate(pattern, rb.position, rb.rotation);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (!shoot)
+		{
+			if (frameCounter < moveBeforeShoot)
+			{
+				rb.position += -transform.forward * Time.deltaTime * speed;
+				frameCounter++;
+			}
+			else
+			{
+				Instantiate(pattern, rb.position, rb.rotation);
+				frameCounter = 0;
+				shoot = true;
+			}
+		}
+		else
+		{
+			if (frameCounter < waitWhileShoot)
+			{
+				frameCounter++;
+			}
+			else
+			{
+				frameCounter = 0;
+				shoot = false;
+			}
+		}
+	}
+}
