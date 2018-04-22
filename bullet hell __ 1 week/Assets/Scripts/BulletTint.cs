1d00dd3b8ea8c72effe2906531042cb92dbba893
Albert <38449884+royaltycomplex@users.noreply.github.com>
2018-04-21 19:59:46 -0600

    lightdark switch w tint

---
+++ bullet hell __ 1 week/Assets/Scripts/BulletTint.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletTint : MonoBehaviour {
+
+	private bool lightDark;
+	private GameObject player;
+
+	private Renderer rend;
+
+	// Use this for initialization
+	void Start () {
+		rend = GetComponentInChildren<Renderer>();
+		player = GameObject.FindWithTag("Player");	
+		if (player != null)
+		{
+			lightDark = player.GetComponent<LightSwitch>().lightDark;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (player != null)
+		{
+			lightDark = player.GetComponent<LightSwitch>().lightDark;
+		}
+
+		if (gameObject.tag == "Light Bullet" && lightDark)
+		{
+			rend.material.SetColor("_TintColor", new Color(0.25f, 0.25f, 0.25f, 0.35f));
+		}
+		else if (gameObject.tag == "Dark Bullet" && !lightDark)
+		{
+			rend.material.SetColor("_TintColor", new Color(0.25f, 0.25f, 0.25f, 0.35f));
+		}
+		else
+		{
+			rend.material.SetColor("_TintColor", new Color(0.5f, 0.5f, 0.5f, 1));
+		}
+	}
+}
--- bullet hell __ 1 week/Assets/Scripts/DestroyByBullet.cs
+++ bullet hell __ 1 week/Assets/Scripts/DestroyByBullet.cs
@@ -4,19 +4,21 @@ using UnityEngine;
 
 public class DestroyByBullet : MonoBehaviour {
 
+	private bool lightDark;
+
 	// Use this for initialization
 	void Start () {
-		
+		lightDark = GetComponent<LightSwitch>().lightDark;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		lightDark = GetComponent<LightSwitch>().lightDark;
 	}
 
 	void OnTriggerEnter(Collider other)
 	{
-		if (other.tag == "Light Bullet" || other.tag == "Dark Bullet" || other.tag == "Enemy")
+		if ((other.tag == "Light Bullet" && !lightDark) || (other.tag == "Dark Bullet" && lightDark) || other.tag == "Enemy")
 		{
 			Destroy(gameObject);
 		}
